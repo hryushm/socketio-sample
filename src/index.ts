48728cb081c56be5c58993679aa191908e7f2a71
ryushima <hryushm@gmail.com>
2019-04-08 15:00:35 +0000

    Add integration with socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import http from "http";
+import socketio from "socket.io";
 import path from "path";
 
 const app = express();
 const httpServer = new http.Server(app);
+const io = socketio(httpServer);
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../src/index.html"));
 });
 
-httpServer.listen(3000, function() {
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+httpServer.listen(3000, () => {
   console.log("listening on *:3000");
 });
